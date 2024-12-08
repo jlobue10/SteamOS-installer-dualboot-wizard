513154adbe66ff8afcb5ed71ab2acdeeb9914970
jlobue10 <jlobue10@gmail.com>
2024-12-07 19:05:34 -0800

    Update steamos-installer-dualboot-wizard.sh
    Make a larger esp partition by default for testing with W11 24H2 dual booting (and potential issues with too small of an efi system partition)

--- steamos-installer-dualboot-wizard.sh
+++ steamos-installer-dualboot-wizard.sh
@@ -79,7 +79,7 @@ DOPARTVERIFY=1
 # Partition table, sfdisk format
 readvar PARTITION_TABLE <<END_PARTITION_TABLE
   label: gpt
-  ${DISK}${DISK_SUFFIX}1: name="esp",      size=    64MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+  ${DISK}${DISK_SUFFIX}1: name="esp",      size=    512MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
   ${DISK}${DISK_SUFFIX}2: name="efi-A",    size=    32MiB, type=EBD0A0A2-B9E5-4433-87C0-68B6B72699C7
   ${DISK}${DISK_SUFFIX}3: name="efi-B",    size=    32MiB, type=EBD0A0A2-B9E5-4433-87C0-68B6B72699C7
   ${DISK}${DISK_SUFFIX}4: name="rootfs-A", size=  5120MiB, type=4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
